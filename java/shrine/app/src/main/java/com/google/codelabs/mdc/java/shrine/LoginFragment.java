468737a0101d79b9732a0c09e6eee5ce301dc372
RamanSB <ramansb100@gmail.com>
2018-10-13 13:22:08 +0100

    Created buttons on login and screen and implemented navigation to ProductGridFragment

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
@@ -2,7 +2,12 @@ package com.google.codelabs.mdc.java.shrine;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.button.MaterialButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +17,46 @@ import android.view.ViewGroup;
  */
 public class LoginFragment extends Fragment {
 
+
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.shr_login_fragment, container, false);
+        MaterialButton nextButton = (MaterialButton) view.findViewById(R.id.next_button);
+        MaterialButton cancelButton = (MaterialButton) view.findViewById(R.id.cancel_button);
+        final TextInputEditText passwordEditText = (TextInputEditText) view.findViewById(R.id.password_edit_text);
+        final TextInputLayout passwordLayoutText = (TextInputLayout) view.findViewById(R.id.password_text_input);
 
-        // Snippet from "Navigate to the next Fragment" section goes here.
+        //Adding click listener to nextButton
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if(!isPasswordValid(passwordEditText.getText())){
+                    passwordLayoutText.setError(getString(R.string.shr_error_password));
+                }else{
+                    passwordLayoutText.setError(null);
+                    ((NavigationHost) getActivity()).navigateTo(new ProductGridFragment(), false);
+                }
+            }
+        });
+
+        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(isPasswordValid(passwordEditText.getText())){
+                    passwordLayoutText.setError(null);
+                }
+                return false;
+            }
+        });
 
         return view;
     }
 
+
+    private boolean isPasswordValid(Editable passwordText) {
+        return passwordText != null && passwordText.length() >= 8;
+    }
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
 }
